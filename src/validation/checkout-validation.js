c3ea41f1ee9005758c2cc2fad0c6d99c4c31db3c
Farhan Wundari <squidwardsus@Squidwardsus.local>
2025-06-29 02:47:18 +0700

    coba 75

--- src/validation/checkout-validation.js
+++ src/validation/checkout-validation.js
@@ -5,6 +5,7 @@ export const checkoutValidation = Joi.object({
   shippingAddress: Joi.string().required().min(10).max(255),
   shippingCity: Joi.string().required(),
   shippingProvince: Joi.string().required(),
+  shippingDistrict: Joi.string().required(),
   shippingPostCode: Joi.string().required().pattern(/^\d+$/),
   // destinationId: Joi.string().required().pattern(/^\d+$/),
   shippingService: Joi.string().required(),
